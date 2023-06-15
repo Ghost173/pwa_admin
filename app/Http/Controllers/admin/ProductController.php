0db92d22281e9530ab9c55e5333efeb8e9386229
ghost173 <jpirakas007@gmail.com>
2023-06-15 19:05:51 +0530

    product active or deactive

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -23,4 +23,31 @@ class ProductController extends Controller
         $products  = ProductList::latest()->get();
         return  view('admin.products.index',compact('products'));
     }
+
+    public function activateproduct($id) {
+        $product = ProductList::findorFail($id)->update([
+            'product_status' => 1,
+            'updated_at' => Carbon::now(),   
+        ]);
+
+        $notification = array(
+            'message' => '	product status was mark as active',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('admin.getallproducts')->with($notification);
+    }
+
+
+    public function deactivateproduct($id) {
+        $product = ProductList::findorFail($id)->update([
+            'product_status' => 0,
+            'updated_at' => Carbon::now(),   
+        ]);
+
+        $notification = array(
+            'message' => '	product status was mark as deactivateproduct',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('admin.getallproducts')->with($notification);
+    }
 }
